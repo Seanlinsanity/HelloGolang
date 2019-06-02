33ed4dddfe5a261f832e1c228d01ac4772a429d0
Seanlinsanity <seanlin1016@gmail.com>
2019-06-02 18:20:51 +0800

    use goroutine

---
+++ goroutine/goroutine.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// var a [10]int
+	for i := 0; i < 1000; i++ {
+		go func(i int) {
+			for {
+				// a[i]++
+				// runtime.Gosched()
+				fmt.Printf("Hello from goroutine %d\n", i)
+			}
+		}(i)
+	}
+	time.Sleep(time.Second * 5)
+	// fmt.Println(a)
+}
